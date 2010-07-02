22bcad442ee9e2c919f5cb94c9f134531e004728
ian <ian@138bc75d-0d04-0410-961f-82ee72b054a4>
2010-07-02 14:55:03 +0000

    syscall: correctly detect argument size
    Correctly detect the argument sizes so we properly sign-extend
    values when passing to syscalls.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/gccgo@161721 138bc75d-0d04-0410-961f-82ee72b054a4

--- libgo/syscalls/syscall.go
+++ libgo/syscalls/syscall.go
@@ -18,13 +18,12 @@ import "unsafe"
 func libc_syscall32(trap int32, a1, a2, a3, a4, a5, a6 int32) int32 __asm__ ("syscall");
 func libc_syscall64(trap int64, a1, a2, a3, a4, a5, a6 int64) int64 __asm__ ("syscall");
 
-// Do a system call.  We look at the size of int to see how to pass
+// Do a system call.  We look at the size of uintptr to see how to pass
 // the arguments, so that we don't pass a 64-bit value when the function
 // expects a 32-bit one.
 func Syscall(trap, a1, a2, a3 uintptr) (r1, r2, err uintptr) {
   var r uintptr;
-  var i int;
-  if unsafe.Sizeof(i) == 4 {
+  if unsafe.Sizeof(r) == 4 {
     r1 := libc_syscall32(int32(trap), int32(a1), int32(a2), int32(a3), 0, 0, 0);
     r = uintptr(r1);
   } else {
@@ -36,8 +35,7 @@ func Syscall(trap, a1, a2, a3 uintptr) (r1, r2, err uintptr) {
 
 func Syscall6(trap, a1, a2, a3, a4, a5, a6 uintptr) (r1, r2, err uintptr) {
   var r uintptr;
-  var i int;
-  if unsafe.Sizeof(i) == 4 {
+  if unsafe.Sizeof(r) == 4 {
     r1 := libc_syscall32(int32(trap), int32(a1), int32(a2), int32(a3),
     			 int32(a4), int32(a5), int32(a6));
     r = uintptr(r1);
