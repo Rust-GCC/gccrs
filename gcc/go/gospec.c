dff2d9e310eedfcc50cad26b72f6b46e3b1c01b2
ian <ian@138bc75d-0d04-0410-961f-82ee72b054a4>
2010-11-03 18:32:27 +0000

    Change gcc-specific files to GPL.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/gccgo@166267 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/go/gospec.c
+++ gcc/go/gospec.c
@@ -1,8 +1,21 @@
 /* gospec.c -- Specific flags and argument handling of the gcc Go front end.
+   Copyright (C) 2009, 2010 Free Software Foundation, Inc.
 
-   Copyright 2009 The Go Authors. All rights reserved.
-   Use of this source code is governed by a BSD-style
-   license that can be found in the LICENSE file.  */
+This file is part of GCC.
+
+GCC is free software; you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free
+Software Foundation; either version 3, or (at your option) any later
+version.
+
+GCC is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+for more details.
+
+You should have received a copy of the GNU General Public License
+along with GCC; see the file COPYING3.  If not see
+<http://www.gnu.org/licenses/>.  */
 
 #include "config.h"
 #include "system.h"
