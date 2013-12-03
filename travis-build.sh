3231aefb4dabe3f932ab9776330ea8938f08038c
Philip Herron <redbrain@gcc.gnu.org>
2013-12-03 16:42:00 +0000

    GCC RUST: travis CI fix

--- travis-build.sh
+++ travis-build.sh
@@ -2,7 +2,16 @@
 BUILD_DIR="gccrs-build-travis"
 rm -rf $BUILD_DIR
 mkdir $BUILD_DIR
+
 cd $BUILD_DIR
-../configure --enable-languages=rust --disable-multilib --disable-bootstrap
-make
+./contrib/download_prerequisites
+../configure \
+    --enable-languages=rust \
+    --disable-multilib \
+    --disable-bootstrap || exit $?
+make || exit $?
 cd -
+
+echo
+echo "Build Complete"
+exit 0
