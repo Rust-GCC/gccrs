3c3612acd501a7a37a39b8f39a36415a6100724e
SimplyTheOther <simplytheother@gmail.com>
2019-07-30 20:33:13 +0800

    Parser improvements

--- gcc/rust/test3-tiny/parse/rs-parse.cc
+++ gcc/rust/test3-tiny/parse/rs-parse.cc
@@ -1,5 +1,25 @@
 #include "rs-parse.h"
 
+/* parsing notes: 
+ *  kinds of "syntactic units" used:
+ *  - statement: expresses an action to be carried out (executed), e.g.:
+ *      function calls
+ *      goto 
+ *      return 
+ *      (maybe) variable definition
+ *      blocks (apparently) - as in {}
+ *      control structures - if, while, do-while, switch
+ *     cannot return a result and are executed only for side effects
+ *     distinction may not exist in rust entirely due to functional influence of lots of things being
+ *      expressions
+ * 
+ *  - expression: stuff that evaluates to a value, e.g.:
+ *      2 + 3
+ *      y * 6 
+ *  
+ * - variable definition (maybe - if not a statement), e.g.:
+ *      y = x + 2 */
+
 namespace Rust {
     void Parser::parse_statement_seq(bool (Parser::*done)()) {
         // Parse statements until done and append to the current stmt list
@@ -115,5 +135,95 @@ namespace Rust {
 
             return const_TokenPtr();
         }
+    }
+
+    // Parses type in variable declaration.
+    bool Parser::parse_type() {
+        const_TokenPtr t = lexer.peek_token();
+
+        switch (t->get_id()) {
+            case INT:
+                lexer.skip_token();
+                return true;
+            case FLOAT:
+                lexer.skip_token();
+                return true;
+            default:
+                unexpected_token(t);
+                return false;
+        }
+    }
+
+    // Parses an if statement. Probably important to study as it seems complex.
+    void Parser::parse_if_statement() {
+        // skip if statement token
+        if (!skip_token(IF)) {
+            skip_after_end();
+            return;
+        }
+
+        // parse expression in statement body
+        parse_expression();
+
+        // skip the "THEN" after expression
+        skip_token(THEN);
+
+        // parse statement sequence? inside if body. Finish at "end if" or "else"
+        parse_statement_seq(&Parser::done_end_or_else);
+
+        const_TokenPtr tok = lexer.peek_token();
+        // if there is an else, parse statement seq inside its body too
+        if (tok->get_id() == ELSE) {
+            // Consume 'else'
+            skip_token(ELSE);
+
+            parse_statement_seq(&Parser::done_end);
+            // Consume 'end'
+            skip_token(END);
+        } else if (tok->get_id() == END) {
+            // Consume 'end'
+            skip_token(END);
+        } else {
+            unexpected_token(tok);
+            skip_after_end();
+        }
+    }
+
+    // Skips all tokens until EOF.
+    void Parser::skip_after_end() {
+        const_TokenPtr t = lexer.peek_token();
+
+        while (t->get_id() != END_OF_FILE && t->get_id() != END) {
+            lexer.skip_token();
+            t = lexer.peek_token();
+        }
+
+        if (t->get_id() == END) {
+            lexer.skip_token();
+        }
+    }
+
+    // Pratt parser impl of parse_expression.
+    bool Parser::parse_expression(int right_binding_power) {
+        const_TokenPtr current_token = lexer.peek_token();
+        lexer.skip_token();
+
+        if (!null_denotation(current_token))
+            return false;
+
+        while (right_binding_power < left_binding_power(lexer.peek_token())) {
+            current_token = lexer.peek_token();
+            lexer.skip_token();
+
+            if (!left_denotation(current_token))
+                return false;
+        }
+
+        return true;
+    }
+
+    // Entry point to parse an expression.
+    bool Parser::parse_expression() {
+        return parse_expression(LBP_LOWEST);
     }
 }
