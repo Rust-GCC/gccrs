6ba79aeda0570b32760e7c2f370b26d71cc20f8f
Yizhe <yizhe@pku.edu.cn>
2021-03-12 11:40:25 +0000

    Improve the s-expression library
    More magic. to_sexp() now can be called on anything, including
    std::vector and std::unique_ptr, and will recursively call to_sexp() on
    the underlying values.

--- gcc/rust/util/rust-sexp.h
+++ gcc/rust/util/rust-sexp.h
@@ -1,7 +1,13 @@
 /* Serialize your objects into S-expression.
    Implement SexpSerializable on your object, then call sexp() as needed. */
 
+#ifndef RUST_SEXP_H
+#define RUST_SEXP_H
+
 #include <string>
+#include <type_traits>
+
+namespace Rust {
 
 class SexpSerializable
 {
@@ -10,54 +16,95 @@ public:
   virtual std::string to_sexp () const = 0;
 };
 
-template <class... Ts>
-static std::string
-sexp_inner ()
+inline std::string
+to_sexp (const std::string &str)
 {
-  return "";
+  return str;
 }
 
-// Accept a string
-template <class... Ts>
-static std::string
-sexp_inner (const std::string &str, Ts... rest)
+inline std::string
+to_sexp (const char *str)
 {
-  return " " + str + sexp_inner (rest...);
+  return std::string (str);
 }
 
-// Accept a SexpSerializable
-template <class... Ts>
-static std::string
-sexp_inner (const SexpSerializable &obj, Ts... rest)
+inline std::string
+to_sexp (const SexpSerializable &obj)
 {
-  return " " + obj.to_sexp ();
+  return obj.to_sexp ();
 }
 
-// Accept a container. Calls to_sexp() on all its objects and concatenates the
-// results.
-template <class U, class... Ts, class = decltype (std::declval<U> ().cbegin ())>
-static std::string
-sexp_inner (const U &container, Ts... rest)
+template <typename T> struct helper
+{
+  using type = void *;
+};
+
+template <typename T> using helper_t = typename helper<T>::type;
+
+template <typename T,
+	  typename std::enable_if<std::is_pointer<T>::value, void *>::type
+	  = nullptr>
+std::string
+to_sexp (const T &ptr)
+{
+  return to_sexp (*ptr);
+}
+
+template <typename T, typename Rust::helper_t<decltype (
+			std::declval<T> ().T::operator* ())> = nullptr>
+std::string
+to_sexp (const T &ptr)
+{
+  return to_sexp (*ptr);
+}
+template <typename T, typename Rust::helper_t<
+			decltype (std::declval<T> ().cbegin ())> = nullptr>
+
+std::string
+to_sexp (const T &container)
 {
   std::string str;
   for (const auto &item : container)
     {
       str += " ";
-      str += item.to_sexp ();
+      str += to_sexp (item);
     }
+  str.erase (str.begin ());
   return str;
+}
+
+template <typename... Ts>
+std::string
+sexp_inner ()
+{
+  return "";
+}
+
+template <typename... Ts, typename U>
+std::string
+sexp_inner (const U &stuff, const Ts &...rest)
+{
+  return " " + to_sexp (stuff) + sexp_inner (rest...);
 }
 
 /* Creates a compound S-expression.
    Accepts an arbitary number of parameters.
-   Parameters can be string, SexpSerializeable, or containers of
-   SexpSerializable. */
-template <class... Ts>
+   The following objects are serializable:
+     - std::string
+     - any class that implements SexpSerializable
+     - containers of serializable objects(e.g. std::vector)
+     - pointers to serializable objects(e.g. std::unique_ptr)
+ */
+template <typename... Ts>
 std::string
-sexp (Ts... rest)
+sexp (const Ts &...rest)
 {
   std::string str = sexp_inner (rest...);
   // Remove the first character, which is an extra whitespace
   str.erase (str.begin ());
   return "(" + str + ")";
 }
+
+} // namespace Rust
+
+#endif
