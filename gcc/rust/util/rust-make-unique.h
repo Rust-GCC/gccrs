cba30e281c78620b65cdea6ed2f01a83ce585f3e
Arthur Cohen <arthur.cohen@embecosm.com>
2022-10-05 09:49:05 +0200

    [15/37] make_unique: Remove system includes

--- gcc/rust/util/rust-make-unique.h
+++ gcc/rust/util/rust-make-unique.h
@@ -19,13 +19,13 @@
 #ifndef RUST_MAKE_UNIQUE_H
 #define RUST_MAKE_UNIQUE_H
 
-#include <memory>
+#include "rust-system.h"
 
 namespace Rust {
 
 template <typename T, typename... Ts>
 std::unique_ptr<T>
-make_unique (Ts &&... params)
+make_unique (Ts &&...params)
 {
   return std::unique_ptr<T> (new T (std::forward<Ts> (params)...));
 }
