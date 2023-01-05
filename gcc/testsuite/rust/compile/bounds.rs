24bf4e725f44a8ca8d759a18e5768c3f65a58d66
mxlol233 <mxlol233@outlook.com>
2023-01-05 19:24:22 +0800

    parse: fix parse for associated type bounds, make the error message aligned with rustc.
    Signed-off-by: Xiao Ma <mxlol233@outlook.com>

--- gcc/testsuite/rust/compile/bounds.rs
+++ gcc/testsuite/rust/compile/bounds.rs
@@ -4,7 +4,21 @@ trait Foo {
 
 trait Copy {}
 
+trait T0 {}
+trait T1 {}
+trait T2 {}
+trait T3 {}
 
-fn c<F: Foo<Bar: Foo>>() where F::Bar: Copy { // { dg-warning "function is never used: 'c'" }
+fn c<F: Foo<Bar: Foo>>() where F::Bar: Copy { // { dg-error "associated type bounds are unstable" }
 }
 
+
+fn d<F: Foo<Bar: Foo // { dg-error "associated type bounds are unstable" }
+
++ T0
+
++ T1
++ T2 +
+
+T3      >>() where F::Bar: Copy { 
+}
