fbfca2158df09d3aa55b81ccc55832b9e87e6bf4
Pierre-Emmanuel Patry <pierre-emmanuel.patry@embecosm.com>
2023-09-15 15:33:00 +0200

    Change unresolved import error text
    The original error text did not match the test nor rustc's behavior.
    
    gcc/rust/ChangeLog:
    
    	* resolve/rust-toplevel-name-resolver-2.0.cc (TopLevel::visit): Update
    	error text to match rustc's one.
    
    gcc/testsuite/ChangeLog:
    
    	* rust/compile/name_resolution14.rs: Update the test with error code.
    
    Signed-off-by: Pierre-Emmanuel Patry <pierre-emmanuel.patry@embecosm.com>

--- gcc/testsuite/rust/compile/name_resolution14.rs
+++ gcc/testsuite/rust/compile/name_resolution14.rs
@@ -2,11 +2,11 @@ pub mod foo {
     pub macro bar() {}
 }
 
-use foo::biz; // { dg-error "unresolved import .foo::biz." }
+use foo::biz; // { dg-error "unresolved import .foo::biz. .E0433." }
 
 use foo::{bar, baz, biz};
-// { dg-error "unresolved import .foo::baz." "" { target *-*-* } .-1 }
-// { dg-error "unresolved import .foo::biz." "" { target *-*-* } .-2 }
+// { dg-error "unresolved import .foo::baz. .E0433." "" { target *-*-* } .-1 }
+// { dg-error "unresolved import .foo::biz. .E0433." "" { target *-*-* } .-2 }
 
 fn main() {
     bar!();
