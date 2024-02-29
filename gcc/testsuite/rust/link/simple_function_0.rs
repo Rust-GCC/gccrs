a92d8efe37cf65506b0642af125376d0b3ac6d3c
Owen Avery <powerboat9.gamer@gmail.com>
2024-02-28 20:14:53 -0500

    disable simple_function_0 test

--- gcc/testsuite/rust/link/simple_function_0.rs
+++ gcc/testsuite/rust/link/simple_function_0.rs
@@ -1,8 +1,10 @@
+// TODO: supply simple_function_1
+// { dg-excess-errors "" { xfail *-*-* } }
 extern crate simple_function_1;
 use simple_function_1::test_func;
 
 fn main() -> i32 {
     let a = test_func(123);
-    // { dg-bogus "call to extern function" "" { xfail *-*-* } .-1 }
+    // \{ d//g-bogus "call to extern function" "" { xfail *-*-* } .-1 }
     a - 124
 }
