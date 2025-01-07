057c7e625e16eb4e7ca18193a25f8fd5bf6a1a5a
Om Swaroop Nayak <146124307+saeitoshi-10@users.noreply.github.com>
2025-01-07 22:19:45 +0530

    Update rust-parse-impl.h

--- gcc/rust/parse/rust-parse-impl.h
+++ gcc/rust/parse/rust-parse-impl.h
@@ -10441,7 +10441,7 @@ Parser<ManagedTokenSource>::parse_pattern ()
     {
       lexer.skip_token ();
       alts.push_back (parse_pattern_no_alt ());
-  }
+    }
 
   while (lexer.peek_token ()->get_id () == PIPE);
 
