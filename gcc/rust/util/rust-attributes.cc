1d219a66bb8624c041d1d4e5e5dd4dbbd9aca012
Owen Avery <powerboat9.gamer@gmail.com>
2025-07-17 18:07:14 -0400

    mangle/link_name fixes

--- gcc/rust/util/rust-attributes.cc
+++ gcc/rust/util/rust-attributes.cc
@@ -79,6 +79,7 @@ static const BuiltinAttrDefinition __definitions[]
      {Attrs::MUST_USE, STATIC_ANALYSIS},
      {Attrs::LANG, HIR_LOWERING},
      {Attrs::LINK_SECTION, CODE_GENERATION},
+     {Attrs::LINK_NAME, CODE_GENERATION},
      {Attrs::NO_MANGLE, CODE_GENERATION},
      {Attrs::REPR, CODE_GENERATION},
      {Attrs::RUSTC_BUILTIN_MACRO, EXPANSION},
