4e49d112a4edcef86dd60d250abcbcd05c848072
Philip Herron <herron.philip@googlemail.com>
2025-09-03 17:35:47 +0100

    gccrs: Revert "nr1.0: Remove support in expansion phase"
    This reverts commit 3e527693cfae6b41c841d6f09a601d2e6d1b2964.
    
    gcc/rust/ChangeLog:
    
    	* expand/rust-macro-expand.cc (MacroExpander::expand_crate): revert
    	* expand/rust-macro-expand.h (struct MacroExpander): likewise
    
    Signed-off-by: Philip Herron <herron.philip@googlemail.com>

--- gcc/rust/expand/rust-macro-expand.cc
+++ gcc/rust/expand/rust-macro-expand.cc
@@ -27,11 +27,9 @@
 #include "rust-macro.h"
 #include "rust-parse.h"
 #include "rust-cfg-strip.h"
+#include "rust-early-name-resolver.h"
 #include "rust-proc-macro.h"
 #include "rust-token-tree-desugar.h"
-
-// flag_assume_builtin_offset_of
-#include "options.h"
 
 namespace Rust {
 
@@ -337,6 +335,9 @@ MacroExpander::expand_invoc (AST::MacroInvocation &invoc,
 void
 MacroExpander::expand_crate ()
 {
+  NodeId scope_node_id = crate.get_node_id ();
+  resolver->get_macro_scope ().push (scope_node_id);
+
   /* fill macro/decorator map from init list? not sure where init list comes
    * from? */
 
