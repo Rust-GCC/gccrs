1dc63fb5586696281c9f412f75bcef4d73e4a4fa
Philip Herron <herron.philip@googlemail.com>
2025-09-03 12:43:22 +0100

    gccrs: Remove the template parameter so clang format stops barfing on this
    Change this formatting, so clang format stops formatting this wierd for me.
    
    gcc/rust/ChangeLog:
    
    	* typecheck/rust-tyty.cc (VariantDef::clone): remove template param
    	(VariantDef::monomorphized_clone): likewise
    
    Signed-off-by: Philip Herron <herron.philip@googlemail.com>

--- gcc/rust/typecheck/rust-tyty.cc
+++ gcc/rust/typecheck/rust-tyty.cc
@@ -1699,8 +1699,7 @@ VariantDef::clone () const
     cloned_fields.push_back ((StructFieldType *) f->clone ());
 
   auto &&discriminant_opt = has_discriminant ()
-			      ? tl::optional<std::unique_ptr<HIR::Expr>> (
-				get_discriminant ().clone_expr ())
+			      ? tl::optional (get_discriminant ().clone_expr ())
 			      : tl::nullopt;
 
   return new VariantDef (id, defid, identifier, ident, type,
@@ -1715,8 +1714,7 @@ VariantDef::monomorphized_clone () const
     cloned_fields.push_back ((StructFieldType *) f->monomorphized_clone ());
 
   auto discriminant_opt = has_discriminant ()
-			    ? tl::optional<std::unique_ptr<HIR::Expr>> (
-			      get_discriminant ().clone_expr ())
+			    ? tl::optional (get_discriminant ().clone_expr ())
 			    : tl::nullopt;
 
   return new VariantDef (id, defid, identifier, ident, type,
