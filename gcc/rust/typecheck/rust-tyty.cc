49e5b0e9fa4883098177b4919ca3192095e49cf2
Philip Herron <herron.philip@googlemail.com>
2025-09-03 16:55:46 +0100

    gccrs: Revert "gccrs: Remove the template parameter
    This reverts commit a50fb38f36506e02139f3ff9343e099c2f5508d7 as it breaks
    gcc5 bootstrap.
    
    gcc/rust/ChangeLog:
    
    	* typecheck/rust-tyty.cc (VariantDef::clone): revert
    	(VariantDef::monomorphized_clone): likewise
    
    Signed-off-by: Philip Herron <herron.philip@googlemail.com>

--- gcc/rust/typecheck/rust-tyty.cc
+++ gcc/rust/typecheck/rust-tyty.cc
@@ -1661,7 +1661,8 @@ VariantDef::clone () const
     cloned_fields.push_back ((StructFieldType *) f->clone ());
 
   auto &&discriminant_opt = has_discriminant ()
-			      ? tl::optional (get_discriminant ().clone_expr ())
+			      ? tl::optional<std::unique_ptr<HIR::Expr>> (
+				get_discriminant ().clone_expr ())
 			      : tl::nullopt;
 
   return new VariantDef (id, defid, identifier, ident, type,
@@ -1676,7 +1677,8 @@ VariantDef::monomorphized_clone () const
     cloned_fields.push_back ((StructFieldType *) f->monomorphized_clone ());
 
   auto discriminant_opt = has_discriminant ()
-			    ? tl::optional (get_discriminant ().clone_expr ())
+			    ? tl::optional<std::unique_ptr<HIR::Expr>> (
+			      get_discriminant ().clone_expr ())
 			    : tl::nullopt;
 
   return new VariantDef (id, defid, identifier, ident, type,
