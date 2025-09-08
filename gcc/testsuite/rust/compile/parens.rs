609179581eaa0b16d219f1e38ac79be9dfa35ec6
0xllx0 <github+elle@weathered-steel.dev>
2025-09-08 19:58:26 +0000

    tests: added extra parens compile test
    Adds a test to verify extra parentheses are discarded, and the source is
    compiled as expected.
    
    Resolves: #2886

---
+++ gcc/testsuite/rust/compile/parens.rs
@@ -0,0 +1,15 @@
+pub fn ret_parens(x: i32) -> i32 {
+    ((x+1))
+}
+
+pub fn arg_ret_parens((x): (i32)) -> (i32) {
+    ((x+1))
+}
+
+pub fn ret_rpit_parens2(x: i32) -> (i32) {
+    ((x+1))
+}
+
+pub fn ret_parens3(x: i32) -> i32 {
+    ((x+1))
+}
