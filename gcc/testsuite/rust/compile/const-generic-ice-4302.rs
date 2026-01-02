37f91c9b756138ab61e53476e7ef12ab98c1339c
jayant chauhan <0001jayant@gmail.com>
2026-01-02 21:04:30 +0530

    Modify error directive in const-generic-ice-4302.rs
    Updated error directive for constant evaluation with generics.

--- gcc/testsuite/rust/compile/const-generic-ice-4302.rs
+++ gcc/testsuite/rust/compile/const-generic-ice-4302.rs
@@ -1,7 +1,6 @@
-// { dg-error "cannot evaluate constant expression with generic parameters" }
-
 pub struct Foo<const N: usize>;
 
+// { dg-error "cannot evaluate constant expression with generic parameters" }
 pub fn foo<const N: usize>() -> Foo<{ N + 1 }> {
     Foo
 }
