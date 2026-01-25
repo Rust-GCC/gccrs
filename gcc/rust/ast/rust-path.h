8dc5ed069d4682228a7070ea04f500d1096f7e29
Owen Avery <powerboat9.gamer@gmail.com>
2026-01-25 14:15:38 -0500

    gccrs: Reduce node id sharing
    Also removes some redundant copies and improves TypePath copying.
    
    gcc/rust/ChangeLog:
    
    	* ast/rust-ast-builder.cc (Builder::trait_bound): Replace copy
    	with move.
    	(Builder::trait_impl): Likewise.
    	(Builder::new_const_param): Replace cloning with reconstruction.
    	* ast/rust-path.h (TypePath::TypePath): Use TypeNoBounds copy
    	constructor.
    	(TypePath::operator=): Use TypeNoBounds copy assignment.
    	* expand/rust-derive-clone.cc (DeriveClone::clone_impl): Produce
    	trait bounds lazily.
    	* expand/rust-derive-copy.cc (DeriveCopy::copy_impl): Likewise.
    	* expand/rust-derive-debug.cc (DeriveDebug::stub_derive_impl):
    	Likewise.
    	* expand/rust-derive-default.cc (DeriveDefault::default_impl):
    	Likewise.
    	* expand/rust-derive-eq.cc (DeriveEq::eq_impls): Likewise.
    	* expand/rust-derive-hash.cc (DeriveHash::hash_impl): Likewise.
    	* expand/rust-derive-ord.cc (DeriveOrd::cmp_impl): Likewise.
    	* expand/rust-derive-partial-eq.cc
    	(DerivePartialEq::partialeq_impls): Likewise.
    	* expand/rust-derive.cc (DeriveVisitor::setup_impl_generics):
    	Likewise.
    	* expand/rust-derive.h (DeriveVisitor::setup_impl_generics):
    	Likewise.
    
    Signed-off-by: Owen Avery <powerboat9.gamer@gmail.com>

--- gcc/rust/ast/rust-path.h
+++ gcc/rust/ast/rust-path.h
@@ -1216,10 +1216,10 @@ public:
 
   // Copy constructor with vector clone
   TypePath (TypePath const &other)
-    : has_opening_scope_resolution (other.has_opening_scope_resolution),
+    : TypeNoBounds (other),
+      has_opening_scope_resolution (other.has_opening_scope_resolution),
       locus (other.locus)
   {
-    node_id = other.node_id;
     segments.reserve (other.segments.size ());
     for (const auto &e : other.segments)
       segments.push_back (e->clone_type_path_segment ());
@@ -1228,7 +1228,7 @@ public:
   // Overloaded assignment operator with clone
   TypePath &operator= (TypePath const &other)
   {
-    node_id = other.node_id;
+    TypeNoBounds::operator= (other);
     has_opening_scope_resolution = other.has_opening_scope_resolution;
     locus = other.locus;
 
