56fd28eebb69a5050c6b21e34474300716763c42
Islam-Imad <islamimad404@gmail.com>
2026-02-03 14:11:48 +0200

    gccrs : parse: Fix ICE when using 'dyn' without trait bound
    The parser was accepting 'dyn' as a standalone type in contexts like:
      static STATIC_1: dyn = 1;
    
    This invalid syntax should be rejected at parse time.
    
    Fixes Rust-GCC/gccrs#3905
    
    gcc/rust/ChangeLog:
    
    	* lex/rust-lex.cc (is_whitespace): refactor if-else to switch
    	* lex/rust-token.h: refactor if-else to switch
    	* parse/rust-parse-impl.hxx: return nullptr when type is in invalid state
    
    gcc/testsuite/ChangeLog:
    
    	* rust/compile/issue-3904.rs: refactor the testcase to match the new thrown error
    
    Signed-off-by: Islam-Imad <islamimad404@gmail.com>

--- gcc/testsuite/rust/compile/issue-3904.rs
+++ gcc/testsuite/rust/compile/issue-3904.rs
@@ -1,4 +1,12 @@
 #![feature(no_core)]
 #![no_core]
 
-static STATIC_1: dyn = *#[serde()]; // { dg-error "found unexpected token .;. in null denotation" "" { target *-*-* } 0 }
+trait A {}
+impl A for i32 {}
+fn main() {
+    let _a: &dyn A = &1;
+}
+
+static STATIC_1: dyn = 1; // {  dg-error "failed to parse TraitObjectType initial bound" "" { target *-*-* }   }
+static STATIC_2: dyn = *#[serde()]; // {  dg-error "failed to parse TraitObjectType initial bound" "" { target *-*-* }   }
+                                    // { dg-error "found unexpected token .;. in null denotation" "" { target *-*-* } .-1 }
