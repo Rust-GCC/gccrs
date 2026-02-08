6e4eb3dcde25f8473cabd5085ff6d7bd75235b84
Islam-Imad <islamimad404@gmail.com>
2026-02-08 12:11:50 +0200

    gccrs : TypeCheck : Enhance missing fields error message
    improved the error message from "a, b, c" to "'a', 'b' and 'c'"
    
    gcc/rust/ChangeLog:
    
    	* typecheck/rust-hir-type-check-struct.cc: enhance missing fields error message
    
    gcc/testsuite/ChangeLog:
    
    	* rust/compile/missing_constructor_fields.rs: update to match the new error message
    
    Signed-off-by: Islam-Imad <islamimad404@gmail.com>

--- gcc/testsuite/rust/compile/missing_constructor_fields.rs
+++ gcc/testsuite/rust/compile/missing_constructor_fields.rs
@@ -9,7 +9,7 @@ struct Foo {
 }
 
 fn main() {
-    let z = Foo { x: 0 , y:1 }; // { dg-error "missing field z in initializer of 'Foo'" }
-    let xz = Foo { y:1 }; // { dg-error "missing fields x, z in initializer of 'Foo'" }
-    let xyz = Foo { }; // { dg-error "missing fields x, y, z in initializer of 'Foo'" }
+    let z = Foo { x: 0 , y:1 }; // { dg-error "missing field 'z' in initializer of 'Foo'" }
+    let xz = Foo { y:1 }; // { dg-error "missing fields 'x' and 'z' in initializer of 'Foo'" }
+    let xyz = Foo { }; // { dg-error "missing fields 'x', 'y' and 'z' in initializer of 'Foo'" }
 }
