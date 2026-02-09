403b38e7b82cfae5fbbb825990b8a3aa7a78f476
Stefan Schulze Frielinghaus <stefansf@gcc.gnu.org>
2026-02-09 17:22:08 +0100

    s390: testsuite: Use arch z14 instead of native
    Pin those tests to z14.  Strictly speaking those are mostly fine for
    z13, too.  However, vectors with 32-bit float elements are only natively
    supported since z14.
    
    gcc/testsuite/ChangeLog:
    
    	* gcc.target/s390/vector/partial/s390-vec-length-epil-1.c:
    	Use arch z14 instead of native.
    	* gcc.target/s390/vector/partial/s390-vec-length-epil-2.c:
    	Ditto.
    	* gcc.target/s390/vector/partial/s390-vec-length-epil-3.c:
    	Ditto.
    	* gcc.target/s390/vector/partial/s390-vec-length-epil-7.c:
    	Ditto.
    	* gcc.target/s390/vector/partial/s390-vec-length-epil-run-1.c:
    	Ditto.
    	* gcc.target/s390/vector/partial/s390-vec-length-epil-run-2.c:
    	Ditto.
    	* gcc.target/s390/vector/partial/s390-vec-length-epil-run-3.c:
    	Ditto.
    	* gcc.target/s390/vector/partial/s390-vec-length-epil-run-7.c:
    	Ditto.
    	* gcc.target/s390/vector/partial/s390-vec-length-full-1.c:
    	Ditto.
    	* gcc.target/s390/vector/partial/s390-vec-length-full-2.c:
    	Ditto.
    	* gcc.target/s390/vector/partial/s390-vec-length-full-3.c:
    	Ditto.
    	* gcc.target/s390/vector/partial/s390-vec-length-full-7.c:
    	Ditto.
    	* gcc.target/s390/vector/partial/s390-vec-length-full-run-1.c:
    	Ditto.
    	* gcc.target/s390/vector/partial/s390-vec-length-full-run-2.c:
    	Ditto.
    	* gcc.target/s390/vector/partial/s390-vec-length-full-run-3.c:
    	Ditto.
    	* gcc.target/s390/vector/partial/s390-vec-length-full-run-7.c:
    	Ditto.
    	* gcc.target/s390/vector/partial/s390-vec-length-small.c:
    	Ditto.

--- gcc/testsuite/gcc.target/s390/vector/partial/s390-vec-length-epil-run-7.c
+++ gcc/testsuite/gcc.target/s390/vector/partial/s390-vec-length-epil-run-7.c
@@ -1,5 +1,5 @@
 /* { dg-do run { target { lp64 && s390_vx } } } */
-/* { dg-options "-march=native -O2 -ftree-vectorize -fno-vect-cost-model -ffast-math -fno-trapping-math" } */
+/* { dg-options "-march=z14 -O2 -ftree-vectorize -fno-vect-cost-model -ffast-math -fno-trapping-math" } */
 
 /* { dg-additional-options "--param=vect-partial-vector-usage=1" } */
 
