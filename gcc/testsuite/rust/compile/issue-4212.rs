cbc18ed4cfa3795dfa2396ae9913593e32545e6a
Pierre-Emmanuel Patry <pierre-emmanuel.patry@embecosm.com>
2026-02-11 17:42:22 +0100

    Make attribute a proper AST node
    Attribute have always been a bit weird, they were part of the AST but did
    not have any visitor function, this create a lot of friction around them
    as their input which may be an expression, a literal and even an
    expression in the future didn't get visited properly during the different
    passes of the compiler.
    
    gcc/rust/ChangeLog:
    
    	* ast/rust-ast-full-decls.h (struct Attribute): Change from here...
    	(class Attribute): ...to a class here.
    	* ast/rust-ast-visitor.cc (DefaultASTVisitor::visit): Add a default
    	visit function for the attributes.
    	* ast/rust-ast-visitor.h: Add a proper visit function declaration for
    	attributes.
    	* ast/rust-ast.cc (Attribute::accept_vis): Add a function to accept a
    	visitor within the attributes.
    	* ast/rust-ast.h (struct Attribute): Add the accept_vis function
    	prototype.
    	(class Attribute): Rename from struct to class.
    	* expand/rust-derive.h: Add an empty implementation for visits to
    	prevent the visitor from recursing into the attributes.
    	* hir/rust-ast-lower-base.cc (ASTLoweringBase::visit): Add missing
    	implementation for attribute visit function. Add a check for derive
    	attributes that would have slipped through the passes.
    	* hir/rust-ast-lower-base.h: Add function prototype.
    	* resolve/rust-early-name-resolver-2.0.cc (Early::visit_attributes):
    	Split the function into two halves.
    	(Early::visit_derive_attribute): Specialized function from
    	visit_attribute for derive attributes.
    	(Early::visit_non_builtin_attribute): Likewise for non builtin
    	attributes.
    	(Early::visit): Rename visit_attributes to visit.
    	* resolve/rust-early-name-resolver-2.0.h (class Early): Update function
    	prototypes.
    
    gcc/testsuite/ChangeLog:
    
    	* rust/compile/issue-4212.rs: Update test with the new errors.
    
    Signed-off-by: Pierre-Emmanuel Patry <pierre-emmanuel.patry@embecosm.com>

--- gcc/testsuite/rust/compile/issue-4212.rs
+++ gcc/testsuite/rust/compile/issue-4212.rs
@@ -3,6 +3,8 @@
 
 #![derive(PartialOrd, PartialEq)]
 // { dg-error "attribute cannot be used at crate level" "" { target *-*-* } .-1 }
+// { dg-error "could not resolve trait .PartialOrd." "" { target *-*-* } .-2 }
+// { dg-error "could not resolve trait .PartialEq." "" { target *-*-* } .-3 }
 pub fn check_ge(a: i32, b: i32) -> bool {
     a >= b
 }
