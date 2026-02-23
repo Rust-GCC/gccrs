4fbec090327dad1957d558f19ce34e16fd742060
Pierre-Emmanuel Patry <pierre-emmanuel.patry@embecosm.com>
2026-02-23 15:08:10 +0100

    Correctly emit warning on invalid attributes
    We were emitting warning even on attribute that were then removed by
    the cfgstrip pass. We cannot move the cfg strip pass before the attribute
    checking because we need to first emit malformed input error messages.
    This means the attribute checking pass must know if an attribute input
    may be removed later down the line.
    
    gcc/rust/ChangeLog:
    
    	* ast/rust-ast.cc (MetaItemPathExpr::to_attribute): Remove cast to
    	literal.
    	(AttrInputMetaItemContainer::separate_cfg_attrs): Remove PathExpr
    	specific code.
    	* expand/rust-cfg-strip.cc (expand_cfg_attrs): Remove the whole
    	attribute if condition's result is false.
    	* util/rust-attributes.cc (AttributeChecker::visit): Remove specific
    	code section for meta item container. Do not check input if
    	configuration does not match condition.
    
    gcc/testsuite/ChangeLog:
    
    	* rust/compile/attr_malformed_path.rs: Filter existing test to x86_64
    	exclusively, add two new tests that appear when visiting the resulting
    	expression.
    
    Signed-off-by: Pierre-Emmanuel Patry <pierre-emmanuel.patry@embecosm.com>

--- gcc/testsuite/rust/compile/attr_malformed_path.rs
+++ gcc/testsuite/rust/compile/attr_malformed_path.rs
@@ -3,4 +3,6 @@
 
 #[cfg_attr(target_arch = "x86_64", path = (target_arch = "x86",    path = "x86.rs"))]
 mod imp {}
-// { dg-error "malformed .path. attribute input" "" { target *-*-* } .-2 }
+// { dg-error "malformed .path. attribute input" "" { target { x86_64-*-* && { lp64 } } } .-2 }
+// { dg-error "cannot find value .target_arch. in this scope" "" { target { x86_64-*-* && { lp64 } } } .-3 }
+// { dg-error "cannot find value .path. in this scope" "" { target { x86_64-*-* && { lp64 } } } .-4 }
