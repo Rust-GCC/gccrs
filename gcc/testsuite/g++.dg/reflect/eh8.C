dac8b0c3ef7280b023369a956e0a9b4d4187c5c2
Jakub Jelinek <jakub@redhat.com>
2026-03-11 11:55:56 +0100

    testsuite: Fix up g++.dg/reflect/eh8.C for Solaris iconv [PR124118]
    I forgot about PR112652 and added a test that relies on iconv actually
    failing when converting from UTF-8 with some emojis to a character set
    without those characters.
    Seems Solaris iconv just transcodes such characters as ? instead of
    reporting an error.
    
    The following patch splits the test into two halves, so that the first one
    is tested on Solaris, and uses dg-skip-if for Solaris like in r16-6209
    in the second half (rather than expecting no diagnostics for Solaris,
    because one can use GNU libiconv on Solaris too).
    
    2026-03-11  Jakub Jelinek  <jakub@redhat.com>
    
    	PR c++/124118
    	* g++.dg/reflect/eh8.C (bar, c): Move second part of tests to ...
    	* g++.dg/reflect/eh10.C: ... here, new test.  Add dg-skip-if for
    	solaris.

--- gcc/testsuite/g++.dg/reflect/eh8.C
+++ gcc/testsuite/g++.dg/reflect/eh8.C
@@ -19,13 +19,4 @@ foo ()
   return true;
 }
 
-consteval bool
-bar ()
-{
-  exception a (u8"\N{GRINNING FACE}\N{GRINNING FACE WITH SMILING EYES}\N{LEFT SPEECH BUBBLE}", ^^foo);
-  const char *b = a.what ();	// { dg-message "in 'constexpr' expansion of 'a.std::meta::exception::what\\\(\\\)" }
-  return true;			// { dg-error "constexpr message: std::meta::exception message could not be successfully transcoded from UTF-8 to ordinary literal encoding" "" { target *-*-* } 0 }
-}
-
 static_assert (foo ());
-constexpr auto c = bar ();
