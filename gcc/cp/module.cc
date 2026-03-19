8e3c5ce5e802225a7416a32a9d9a3c2bb2380874
Nathaniel Shead <nathanieloshead@gmail.com>
2026-03-19 22:58:11 +1100

    c++/modules: Support section attributes [PR122786]
    This is a minimal fix to not just silently drop the section attribute on
    stream-in.  Ideally we should be using cplus_decl_attributes for this,
    but it's not yet ready for modules (many attributes behave weirdly when
    applied not during parsing).  We should also be checking for
    incompatible sections on stream-in in is_matching_decl, but that would
    also be better handled more generally so I'm leaving that out of this
    patch.
    
    Instead this minimally fixes the issue by just re-applying the section
    name from the attributes we've streamed in.
    
    	PR c++/122786
    
    gcc/cp/ChangeLog:
    
    	* module.cc (trees_in::decl_value): Set section name from
    	attribute.
    
    gcc/testsuite/ChangeLog:
    
    	* g++.dg/modules/attrib-5_a.C: New test.
    	* g++.dg/modules/attrib-5_b.C: New test.
    
    Signed-off-by: Nathaniel Shead <nathanieloshead@gmail.com>
    Reviewed-by: Jason Merrill <jason@redhat.com>

--- gcc/cp/module.cc
+++ gcc/cp/module.cc
@@ -9031,6 +9031,17 @@ trees_in::decl_value ()
 	  && decl_tls_wrapper_p (decl))
 	note_vague_linkage_fn (decl);
 
+      /* Apply relevant attributes.
+	 FIXME should probably use cplus_decl_attributes for this,
+	 but it's not yet ready for modules.  */
+
+      if (VAR_OR_FUNCTION_DECL_P (inner))
+	if (tree attr = lookup_attribute ("section", DECL_ATTRIBUTES (inner)))
+	  {
+	    tree section_name = TREE_VALUE (TREE_VALUE (attr));
+	    set_decl_section_name (inner, TREE_STRING_POINTER (section_name));
+	  }
+
       /* Setup aliases for the declaration.  */
       if (tree alias = lookup_attribute ("alias", DECL_ATTRIBUTES (decl)))
 	{
