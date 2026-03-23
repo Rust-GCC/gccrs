cd8412c8f899fc4c2ef063dfe5a01ed772de3cca
Owen Avery <powerboat9.gamer@gmail.com>
2026-03-22 21:00:16 -0400

    Improve unit struct matching
    Unit structs weren't *actually* working in match statements -- we don't
    currently handle identifier pattern vs path pattern disambiguation.
    
    gcc/rust/ChangeLog:
    
    	* checks/errors/rust-hir-pattern-analysis.cc
    	(PlaceInfo::specialize): Handle VariantType::UNIT.
    	(WitnessPat::to_string): Likewise.
    	(WitnessMatrix::apply_constructor): Likewise.
    	* typecheck/rust-hir-type-check-item.cc (TypeCheckItem::visit):
    	Conditionally create VariantType::UNIT variants.
    	* typecheck/rust-hir-type-check-pattern.cc
    	(TypeCheckPattern::visit): Fix typo, handle VariantType::UNIT.
    	* typecheck/rust-tyty-variance-analysis.cc
    	(GenericTyVisitorCtx::process_type): Handle VariantType::UNIT.
    	* typecheck/rust-tyty.cc (BaseType::contains_infer): Likewise.
    	(BaseType::is_concrete): Likewise.
    	(VariantDef::variant_type_string): Likewise.
    	(VariantDef::VariantDef): Likewise.
    	* typecheck/rust-tyty.h (VariantDef::VariantType::UNIT): New
    	enumerator.
    
    gcc/testsuite/ChangeLog:
    
    	* rust/compile/match-struct-path.rs: Increase test coverage.
    
    Signed-off-by: Owen Avery <powerboat9.gamer@gmail.com>

--- gcc/rust/typecheck/rust-tyty.cc
+++ gcc/rust/typecheck/rust-tyty.cc
@@ -726,7 +726,9 @@ BaseType::contains_infer () const
 	{
 	  bool is_num_variant
 	    = variant->get_variant_type () == VariantDef::VariantType::NUM;
-	  if (is_num_variant)
+	  bool is_unit_variant
+	    = variant->get_variant_type () == VariantDef::VariantType::UNIT;
+	  if (is_num_variant || is_unit_variant)
 	    continue;
 
 	  for (auto &field : variant->get_fields ())
@@ -831,7 +833,9 @@ BaseType::is_concrete () const
 	{
 	  bool is_num_variant
 	    = variant->get_variant_type () == VariantDef::VariantType::NUM;
-	  if (is_num_variant)
+	  bool is_unit_variant
+	    = variant->get_variant_type () == VariantDef::VariantType::UNIT;
+	  if (is_num_variant || is_unit_variant)
 	    continue;
 
 	  for (auto &field : variant->get_fields ())
@@ -1508,6 +1512,8 @@ VariantDef::variant_type_string (VariantType type)
       return "tuple";
     case STRUCT:
       return "struct";
+    case UNIT:
+      return "unit struct";
     }
   rust_unreachable ();
   return "";
@@ -1532,7 +1538,8 @@ VariantDef::VariantDef (HirId id, DefId defid, std::string identifier,
     discriminant (std::move (discriminant)), fields (fields)
 {
   rust_assert ((type == VariantType::NUM && fields.empty ())
-	       || (type == VariantType::TUPLE || type == VariantType::STRUCT));
+	       || (type == VariantType::UNIT && fields.empty ())
+	       || type == VariantType::TUPLE || type == VariantType::STRUCT);
 }
 
 VariantDef &
