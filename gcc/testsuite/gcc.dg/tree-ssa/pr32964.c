39d3e640c944edf5ce537e95eb1d1a338363f207
Alexandre Oliva <oliva@adacore.com>
2026-03-30 21:57:56 -0300

    testsuite: select -mfpmath=sse for pr32964.c on x86
    When using the 387 FPU, we don't perform the expected optimization of
    unions to floats.  Setting -fexcess-precision=standard. doesn't fix it
    for x86, but selecting -mfpmath=sse and forcing sse enabled does.
    
    
    for  gcc/testsuite/ChangeLog
    
    	* gcc.dg/tree-ssa/pr32964.c: Select the SSE FPU on x86.

--- gcc/testsuite/gcc.dg/tree-ssa/pr32964.c
+++ gcc/testsuite/gcc.dg/tree-ssa/pr32964.c
@@ -1,5 +1,8 @@
 /* { dg-do compile } */
 /* { dg-options "-O1 -fdump-tree-optimized" } */
+/* We don't get the expected optimization any more when x87's excess precision
+   is available.  */
+/* { dg-additional-options "-msse -mfpmath=sse" { target x86 } } */
 
 union A
 {
