984adab6a054d86170d55de7ae5a50df99cbe8e3
Torbjörn SVENSSON <torbjorn.svensson@foss.st.com>
2026-03-31 16:42:38 +0200

    testsuite: disable loop peeling check for arm-none-eabi [PR124705]
    Unaligned accesses or peeling for alignment is ok for arm-none-eabi.
    
    gcc/testsuite/ChangeLog:
    
    	PR target/124705
    	* gcc.dg/tree-ssa/gen-vect-26.c: Disable loop peeling check for
    	arm-none-eabi.
    	* gcc.dg/tree-ssa/gen-vect-28.c: Likewise.
    
    Signed-off-by: Torbjörn SVENSSON <torbjorn.svensson@foss.st.com>

--- gcc/testsuite/gcc.dg/tree-ssa/gen-vect-26.c
+++ gcc/testsuite/gcc.dg/tree-ssa/gen-vect-26.c
@@ -32,5 +32,5 @@ int main ()
 
 /* { dg-final { scan-tree-dump-times "vectorized 1 loops" 1 "vect" { target { ! { avr-*-* pru-*-* } } } } } */
 /* IBM Z does not require special alignment for vectorization.  */
-/* { dg-final { scan-tree-dump-times "Vectorizing an unaligned access" 0 "vect" { target { ! { avr-*-* pru-*-* s390*-*-* } } } } } */
-/* { dg-final { scan-tree-dump-times "Alignment of access forced using peeling" 1 "vect" { target { ! { avr-*-* pru-*-* s390*-*-* } } } } } */
+/* { dg-final { scan-tree-dump-times "Vectorizing an unaligned access" 0 "vect" { target { ! { arm-none-eabi avr-*-* pru-*-* s390*-*-* } } } } } */
+/* { dg-final { scan-tree-dump-times "Alignment of access forced using peeling" 1 "vect" { target { ! { arm-none-eabi avr-*-* pru-*-* s390*-*-* } } } } } */
