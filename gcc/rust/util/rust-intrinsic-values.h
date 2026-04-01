52e6db17f59de69bbcb4ba38d181195931e00b3e
Mohamed Ali <mohmedali1462005@gmail.com>
2026-04-01 19:26:25 +0200

    gccrs: intrinsic: Add cttz and cttz_nonzero intrinsics
    This commit introduces the cttz and cttz_nonzero intrinsics for counting leading zeros
    in integer types.
    
    The implementation includes handlers for both intrinsics, which validate input types and utilize
    
    Addresses: Rust-GCC#658
    
    gcc/rust/ChangeLog:
    
    	* backend/rust-compile-intrinsic.cc: Register cttz and cttz_nonzero intrinsics.
    	* backend/rust-intrinsic-handlers.cc (cttz_handler): Implemnt cttz_handler.
    	(cttz_nonzero_handler): Implemnt cttz_nonzero_handler.
    	* backend/rust-intrinsic-handlers.h (cttz_handler): Add decl for cttz_handler.
    	(cttz_nonzero_handler): Add decl for cttz_handler.
    	* util/rust-intrinsic-values.h: map cttz_nonzero to its name.
    
    Signed-off-by: Mohamed Ali <mohmedali1462005@gmail.com>

--- gcc/rust/util/rust-intrinsic-values.h
+++ gcc/rust/util/rust-intrinsic-values.h
@@ -83,6 +83,7 @@ public:
   static constexpr auto &CTLZ = "ctlz";
   static constexpr auto &CTLZ_NONZERO = "ctlz_nonzero";
   static constexpr auto &CTTZ = "cttz";
+  static constexpr auto &CTTZ_NONZERO = "cttz_nonzero";
   static constexpr auto &BSWAP = "bswap";
   static constexpr auto &BITREVERSE = "bitreverse";
   static constexpr auto &TYPE_ID = "type_id";
