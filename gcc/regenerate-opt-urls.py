e0696b89157d79781e9c438d4ddfda9a41909983
Sandra Loosemore <sloosemore@baylibre.com>
2026-04-05 17:09:37 +0000

    doc: Make regenerate-opt-urls.py handle two-dash options [PR124784]
    I was puzzled as to why the generated config/picolib.opt.urls
    contained no entries.  It turns out this was a general problem with
    regenerate-opt-urls.py failing to handle options starting with two dashes,
    which are listed with one dash in the .opt file but zero dashes (same as
    one-dash options) in the index entries.  The solution here is to remove one
    of the dashes when canonicalizing the option name for lookup in the index.
    
    gcc/ChangeLog
    	PR other/124784
    	* regenerate-opt-urls.py (canonicalize_option_name):  Remove one
    	of the dashes from option names starting with two dashes.
    	(test_parse_option_index): Add a self-test for this.
    	* common.opt.urls: Regenerated.
    	* config/picolibc.opt.urls: Regenerated.
    
    gcc/ada/ChangeLog
    	PR other/124784
    	* gcc-interface/lang.opt.urls: Regenerated.
    
    gcc/c-family/ChangeLog
    	PR other/124784
    	* c.opt.urls: Regenerated.
    
    gcc/d/ChangeLog
    	PR other/124784
    	* lang.opt.urls: Regenerated.
    
    gcc/fortran/ChangeLog
    	PR other/124784
    	* lang.opt.urls: Regenerated.
    
    gcc/m2/ChangeLog
    	PR other/124784
    	* lang.opt.urls: Regenerated.

--- gcc/regenerate-opt-urls.py
+++ gcc/regenerate-opt-urls.py
@@ -46,6 +46,12 @@ import unittest
 def canonicalize_option_name(option_name):
     if option_name.endswith('='):
         option_name = option_name[0:-1]
+    # Options starting with two dashes are listed with one dash in the
+    # .opt file, plus we have already added the implicit dash to option_name.
+    # But the index entries have zero dashes, so we have to remove one of the
+    # dashes again to match them.
+    if option_name[:2] == '--':
+        option_name = option_name[1:]
     return option_name
 
 
@@ -175,6 +181,8 @@ class TestParsingIndex(unittest.TestCase):
                          {'gcc/Optimize-Options.html#index-O0'})
         self.assertEqual(index.get_url_suffixes('-Wframe-larger-than='),
                          {'gcc/Warning-Options.html#index-Wframe-larger-than_003d'})
+        self.assertEqual(index.get_url_suffixes('--static-pie'),
+                         {'gcc/Link-Options.html#index-static-pie'})
 
         # Check an option with duplicates: '-march'
         # The url_suffixes will be of the form
