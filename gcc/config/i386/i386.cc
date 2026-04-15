9579da6e71719b8784943b71d22c7f92d954d2ba
H.J. Lu <hjl.tools@gmail.com>
2026-04-15 13:40:18 +0800

    x86: Fix a typo in comments
    Since x86_64_preserve_none_int_parameter_registers is used in
    ix86_function_arg_regno_p, not ix86_function_value_regno_p, replace
    ix86_function_value_regno_p with ix86_function_arg_regno_p in comments.
    
    	* config/i386/i386.cc
    	(x86_64_preserve_none_int_parameter_registers): Replace
    	ix86_function_value_regno_p with ix86_function_arg_regno_p in
    	comments.
    
    Signed-off-by: H.J. Lu <hjl.tools@gmail.com>

--- gcc/config/i386/i386.cc
+++ gcc/config/i386/i386.cc
@@ -336,7 +336,7 @@ static int const x86_64_ms_abi_int_parameter_registers[4] =
 };
 
 /* Similar as Clang's preserve_none function parameter passing.
-   NB: Use DI_REG and SI_REG, see ix86_function_value_regno_p.  */
+   NB: Use DI_REG and SI_REG, see ix86_function_arg_regno_p.  */
 
 static int const x86_64_preserve_none_int_parameter_registers[6] =
 {
