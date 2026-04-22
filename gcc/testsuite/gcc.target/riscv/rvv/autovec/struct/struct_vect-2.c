49ff5bcc944b65f8f637d541868de6af1f1480fe
Andrew Pinski <andrew.pinski@oss.qualcomm.com>
2026-04-22 13:29:26 -0600

    [PR target/124029][RISC-V] Adjust cost of comparisons
    > Given this is a relatively straightforward define_split, it is likely a good
    > case for Austin to chase down.
    
    Actually it is easier than that
    The middle-end has a costing mechism for this already:
    ```
    ;; cmp: le, old cst: (const_int 268435455 [0xfffffff]) new cst: (const_int 268435456 [0x10000000])
    ;; old cst cost: 4, new cst cost: 4
    ```
    
    You need to implement a COMPARE cost part of the riscv_rtx_costs like it is
    done for aarch64_rtx_costs.
    
    It won't be 100% exact because in riscv case there is no COMPARE instruction.
    But at least it might be more about the costs of generating which constant and
    all.
    
    	PR target/124029
    gcc/
    	* config/riscv/riscv.cc (riscv_rtx_costs): Improve costing of COMPARE
    	nodes.
    
    gcc/testsuite
    	* gcc.target/riscv/pr124029.c: New test.
    	* gcc.target/riscv/rvv/autovec/struct/struct_vect-2.c: Adjust
    	expected output.
    
    	    Co-authored-by: Jeff Law  <jeffrey.law@oss.qualcomm.com>

--- gcc/testsuite/gcc.target/riscv/rvv/autovec/struct/struct_vect-2.c
+++ gcc/testsuite/gcc.target/riscv/rvv/autovec/struct/struct_vect-2.c
@@ -8,7 +8,7 @@
 /* { dg-final { scan-assembler-times {vlseg3e16\.v} 8 } } */
 /* { dg-final { scan-assembler-times {vlseg4e16\.v} 8 } } */
 /* { dg-final { scan-assembler-times {vlseg5e16\.v} 8 } } */
-/* { dg-final { scan-assembler-times {vlseg6e16\.v} 4 } } */
+/* { dg-final { scan-assembler-times {vlseg6e16\.v} 8 } } */
 /* { dg-final { scan-assembler-times {vlseg7e16\.v} 4 } } */
 /* { dg-final { scan-assembler-times {vlseg8e16\.v} 4 } } */
 /* { dg-final { scan-assembler-times {vsseg2e16\.v} 8 } } */
@@ -16,7 +16,7 @@
 /* { dg-final { scan-assembler-times {vsseg4e16\.v} 4 } } */
 /* { dg-final { scan-assembler-times {vsseg5e16\.v} 4 } } */
 /* { dg-final { scan-assembler-times {vsseg6e16\.v} 4 } } */
-/* { dg-final { scan-assembler-times {vsseg7e16\.v} 2 } } */
+/* { dg-final { scan-assembler-times {vsseg7e16\.v} 4 } } */
 /* { dg-final { scan-assembler-times {vsseg8e16\.v} 2 } } */
 /* { dg-final { scan-assembler-times {vsetivli\s+zero,\s*8,\s*e16,\s*m1,\s*t[au],\s*m[au]} 14 } } */
 /* { dg-final { scan-assembler-not {vsetvli} } } */
