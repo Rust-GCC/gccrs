a2d74fe9c932874aa0a295f6c2b95410186c98a2
Jakub Jelinek <jakub@redhat.com>
2026-04-27 10:11:20 +0200

    testsuite: Fix up bitint-95.c test [PR124988]
    I forgot to add the usual guards of bitint tests to bitint-95.c test
    (which were done even in the 4 other tests from the same commit).
    
    2026-04-27  Jakub Jelinek  <jakub@redhat.com>
    
    	PR tree-optimization/124988
    	* gcc.dg/torture/bitint-95.c: Add bitint effective targets and
    	guard parts of test which need _BitInt(192) support with
    	__BITINT_MAXWIDTH__ >= 192.

--- gcc/testsuite/gcc.dg/torture/bitint-95.c
+++ gcc/testsuite/gcc.dg/torture/bitint-95.c
@@ -1,7 +1,8 @@
 /* PR tree-optimization/124988 */
-/* { dg-do run } */
+/* { dg-do run { target bitint } } */
 
 int a;
+#if __BITINT_MAXWIDTH__ >= 192
 unsigned _BitInt(132) b;
 
 unsigned _BitInt(128)
@@ -12,11 +13,14 @@ foo (unsigned _BitInt(192) d)
     __atomic_sub_fetch (&a, 1, 0);
   return b >> 8;
 }
+#endif
 
 int
 main ()
 {
+#if __BITINT_MAXWIDTH__ >= 192
   unsigned _BitInt(128) x = foo (0x1100ffeeddccbbaa998877665544332211wb);
   if (x != 0x100ffeeddccbbaa9988776655443322wb)
     __builtin_abort ();
+#endif
 }
