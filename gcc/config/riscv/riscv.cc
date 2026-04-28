e712b42c5e01d70bc9868c5c7e3953c531ec08e3
Maximilian Ciric <max.ciric@gmail.com>
2026-04-28 11:00:45 -0600

    [PATCH 2/3] RISC-V: Enable address tagging on 64-bit targets supporting pointer masking.
    Implements TARGET_MEMTAG_CAN_TAG_ADDRESSES and TARGET_MEMTAG_TAG_BITSIZE
    for the RISC-V back end, allowing -fsanitize=hwaddress if the target
    machine supports the pointer masking extension.
    
    ------
    
    gcc/ChangeLog:
    
    	* config/riscv/riscv.cc (riscv_can_tag_addresses): New function.
    	(RISCV_HWASAN_TAG_SIZE): New definition.
    	(riscv_memtag_tag_bitsize): New function.
    	(TARGET_MEMTAG_CAN_TAG_ADDRESSES): New definition.
    	(TARGET_MEMTAG_TAG_BITSIZE): Likewise.

--- gcc/config/riscv/riscv.cc
+++ gcc/config/riscv/riscv.cc
@@ -16544,6 +16544,38 @@ riscv_prefetch_offset_address_p (rtx x, machine_mode mode)
   return true;
 }
 
+
+/* Implement TARGET_MEMTAG_CAN_TAG_ADDRESSES.
+ * Enables -fsanitize=hwaddress if true.  */
+
+bool
+riscv_can_tag_addresses ()
+{
+  /* Tagging is possible when a pointer masking extension is available.  */
+  bool target_has_pointer_masking_p = TARGET_SMMPM
+				   || TARGET_SMNPM
+				   || TARGET_SSNPM
+				   || TARGET_SSPM
+				   || TARGET_SUPM;
+  return TARGET_64BIT && target_has_pointer_masking_p;
+}
+
+
+/* libsanitizer expects 8 tag bits.  */
+
+#define RISCV_HWASAN_TAG_SIZE 8
+
+
+/* Implement TARGET_MEMTAG_TAG_BITSIZE.
+ * Communicates how many unused pointer bits are used for tagging.  */
+
+unsigned char
+riscv_memtag_tag_bitsize ()
+{
+  return RISCV_HWASAN_TAG_SIZE;
+}
+
+
 /* Initialize the GCC target structure.  */
 #undef TARGET_ASM_ALIGNED_HI_OP
 #define TARGET_ASM_ALIGNED_HI_OP "\t.half\t"
@@ -16962,6 +16994,12 @@ riscv_prefetch_offset_address_p (rtx x, machine_mode mode)
 #undef TARGET_GET_FUNCTION_VERSIONS_DISPATCHER
 #define TARGET_GET_FUNCTION_VERSIONS_DISPATCHER \
   riscv_get_function_versions_dispatcher
+
+#undef TARGET_MEMTAG_CAN_TAG_ADDRESSES
+#define TARGET_MEMTAG_CAN_TAG_ADDRESSES riscv_can_tag_addresses
+
+#undef TARGET_MEMTAG_TAG_BITSIZE
+#define TARGET_MEMTAG_TAG_BITSIZE riscv_memtag_tag_bitsize
 
 #undef TARGET_DOCUMENTATION_NAME
 #define TARGET_DOCUMENTATION_NAME "RISC-V"
