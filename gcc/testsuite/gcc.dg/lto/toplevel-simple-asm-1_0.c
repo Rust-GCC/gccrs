6601cdc2c9d3ec6b9c79f023a8bf83114d5d8b79
Torbjörn SVENSSON <torbjorn.svensson@foss.st.com>
2026-05-11 09:44:51 +0200

    testsuite: arm: adjust inline assembler for arm-none-eabi
    The fix applied to toplevel-extended-asm-1_0.c in r16-7892-gb02f9495dcf635,
    that defines the symbol as a function, also needs to be applied to
    toplevel-simple-asm-1_0.c.
    
    gcc/testsuite/ChangeLog:
    
    	* gcc.dg/lto/toplevel-simple-asm-1_0.c: Adjust inline assembler
    	for arm-none-eabi.
    
    Signed-off-by: Torbjörn SVENSSON <torbjorn.svensson@foss.st.com>

--- gcc/testsuite/gcc.dg/lto/toplevel-simple-asm-1_0.c
+++ gcc/testsuite/gcc.dg/lto/toplevel-simple-asm-1_0.c
@@ -4,8 +4,23 @@
 void asm_fn();
 void asm_fn_used();
 
+#ifdef __ELF__
+#ifdef __sparc__
+#define TYPE_PFX "#"
+#elif defined (__arm__) || defined (__aarch64__)
+#define TYPE_PFX "%"
+#else
+#define TYPE_PFX "@"
+#endif
+#endif
+
+#ifdef TYPE_PFX
+asm(".global asm_fn\n.type asm_fn, " TYPE_PFX "function\nasm_fn:");
+asm(".global asm_fn_used\n.type asm_fn_used, " TYPE_PFX "function\nasm_fn_used:");
+#else
 asm(".global asm_fn\nasm_fn:");
 asm(".global asm_fn_used\nasm_fn_used:");
+#endif
 
 
 __attribute__((noinline))
