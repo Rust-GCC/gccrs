63874092cb70e723814a8a25dc2a0c17e6d63adb
Alexandre Oliva <oliva@adacore.com>
2026-05-12 03:29:10 -0300

    testsuite: riscv: reset -march for tests with -mcpu
    The tests fail when --target_board sets -march to a 32-bit
    architecture.  Override that -march by resetting it, so that the arch
    implied by -mcpu prevails.
    
    
    for  gcc/testsuite/ChangeLog
    
    	* gcc.target/riscv/pr114139.c: Reset arch.
    	* gcc.target/riscv/pr118170.c: Likewise.

--- gcc/testsuite/gcc.target/riscv/pr118170.c
+++ gcc/testsuite/gcc.target/riscv/pr118170.c
@@ -1,5 +1,5 @@
 /* { dg-do "compile" { target { ! riscv_abi_e } } } */
-/* { dg-options "-O2 -mcpu=tt-ascalon-d8 -mabi=lp64d" } */
+/* { dg-options "-O2 -mcpu=tt-ascalon-d8 -march=unset -mabi=lp64d" } */
 _Float16 f;
 
 void
