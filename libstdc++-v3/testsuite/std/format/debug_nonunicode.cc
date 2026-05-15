c30a026bebebd19db124f60412f3ddcab291c900
Tomasz Kamiński <tkaminsk@redhat.com>
2026-05-15 11:53:51 +0200

    libstdc++: Use IANA name for ISO-8859-1 in format tests.
    Use IANA name for ISO-8859-1 as "-fexec-charset=" and add dg-require-iconv,
    to make sure it is supported.
    
    libstdc++-v3/ChangeLog:
    
    	* testsuite/std/format/debug_nonunicode.cc: Pass ISO-8859-1 as
    	exec-charset and make sure that it is supported.
    	* testsuite/std/format/fill_nonunicode.cc: Likewise.

--- libstdc++-v3/testsuite/std/format/debug_nonunicode.cc
+++ libstdc++-v3/testsuite/std/format/debug_nonunicode.cc
@@ -1,4 +1,5 @@
-// { dg-options "-fexec-charset=ISO8859-1" }
+// { dg-options "-fexec-charset=ISO-8859-1" }
+// { dg-require-iconv "ISO-8859-1" }
 // { dg-do run { target c++23 } }
 // { dg-add-options no_pch }
 
