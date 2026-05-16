18d08c0e6115e6c34eaed73f242ccf210a455240
Jakub Jelinek <jakub@redhat.com>
2026-05-16 10:51:39 +0200

    match.pd: Enable some __builtin_bswap* optimizations even for __builtin_bitreverse* [PR50481]
    Most of the bswap optimizations equally apply also to bitreverse builtins.
    The following patch enables those.
    
    2026-05-16  Jakub Jelinek  <jakub@redhat.com>
    
    	PR target/50481
    	* match.pd (BITREVERSE): New define_operator_list.  Use it next to
    	BSWAP for a subset of bswap simplifications.
    
    	* gcc.dg/builtin-bitreverse-4.c: New test.
    
    Reviewed-by: Andrew Pinski <andrew.pinski@oss.qualcomm.com>

---
+++ gcc/testsuite/gcc.dg/builtin-bitreverse-4.c
@@ -0,0 +1,54 @@
+/* { dg-do compile { target int32 } } */
+/* { dg-options "-O2 -fdump-tree-optimized" } */
+
+#define BS(X) __builtin_bitreverse32 (X)
+
+unsigned
+foo1 (unsigned a)
+{
+  return BS (~ BS (a));
+}
+
+unsigned
+foo2 (unsigned a)
+{
+  return BS (BS (a) & 0xA0000);
+}
+
+unsigned
+foo3 (unsigned a)
+{
+  return BS (BS (a) | 0xA0000);
+}
+
+unsigned
+foo4 (unsigned a)
+{
+  return BS (BS (a) ^ 0xA0000);
+}
+
+unsigned
+foo5 (unsigned a, unsigned b)
+{
+  return BS (BS (a) & BS (b));
+}
+
+unsigned
+foo6 (unsigned a, unsigned b)
+{
+  return BS (BS (a) | BS (b));
+}
+
+unsigned
+foo7 (unsigned a, unsigned b)
+{
+  return BS (BS (a) ^ BS (b));
+}
+
+unsigned
+foo8 (unsigned a)
+{
+  return BS (BS (BS (BS (a))));
+}
+
+/* { dg-final { scan-tree-dump-not "__builtin_bitreverse" "optimized" } } */
