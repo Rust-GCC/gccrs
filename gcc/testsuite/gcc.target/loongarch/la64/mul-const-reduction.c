232f2ca5dd7f15a7ea4320b483803b028d9c5d5f
Jeff Law <jeffrey.law@oss.qualcomm.com>
2026-05-30 11:01:14 -0600

    Fix scan-asm failure for loongarch64 after recent ext-dce changes
    The recent changes to ext-dce to promote narrow operations into wider modes
    when the extended bits are dead slightly twiddled the output for a loongarch64
    test.  This just adjusts the expected output.
    
    Pushing to the trunk.
    
    gcc/testsuite
    
    	* gcc.target/loongarch/la64/mul-const-reduction.c: Adjust expected
    	output.

--- gcc/testsuite/gcc.target/loongarch/la64/mul-const-reduction.c
+++ gcc/testsuite/gcc.target/loongarch/la64/mul-const-reduction.c
@@ -1,6 +1,6 @@
 /* { dg-do compile } */
 /* { dg-options "-O2 -mtune=la464" } */
-/* { dg-final { scan-assembler "alsl\.w" } } */
+/* { dg-final { scan-assembler "alsl\.d" } } */
 /* { dg-final { scan-assembler "slli\.w" } } */
 /* { dg-final { scan-assembler-not "mul\.w" } } */
 
