c3699aaef36a2e3e90a114acedb8436b32fe96b7
Kyrylo Tkachov <ktkachov@nvidia.com>
2026-06-15 00:00:00 +0000

    testsuite: Fix multiply scan regex in mult-negate-zeroone-1.C
    Fix the escaping in the scan in hte testscase.
    Applying as obvious after testing on x86-linux
    
    Signed-off-by: Kyrylo Tkachov <ktkachov@nvidia.com>
    
    gcc/testsuite/ChangeLog:
    
    	PR tree-optimization/125750
    	* g++.dg/tree-ssa/mult-negate-zeroone-1.C: Fix escaping.

--- gcc/testsuite/g++.dg/tree-ssa/mult-negate-zeroone-1.C
+++ gcc/testsuite/g++.dg/tree-ssa/mult-negate-zeroone-1.C
@@ -8,5 +8,5 @@ long f2 (long x, long y) { return (long) (x > 5) * -(long) (y > 5); }
 unsigned f3 (unsigned x, unsigned y) { return (x > 5u) * -(unsigned) (y > 5u); }
 int f4 (int x, int y) { return -(y > 5) * (x > 5); }
 
-// { dg-final { scan-tree-dump-not " \* " "optimized" } }
+// { dg-final { scan-tree-dump-not { \* } "optimized" } }
 // { dg-final { scan-tree-dump-times " & " 4 "optimized" } }
