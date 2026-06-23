325006ba1db93102177052729959c13f7d04a35a
Pan Li <pan2.li@intel.com>
2026-06-23 21:11:10 +0800

    RISC-V: Add testcase for unsigned scalar SAT_MUL form 14
    The form 14 of unsigned scalar SAT_MUL has supported from
    the previous change.  Thus, add the test cases to make sure
    it works well
    
    gcc/testsuite/ChangeLog:
    
    	* gcc.target/riscv/sat/sat_arith.h: Add test helper macros.
    	* gcc.target/riscv/sat/sat_u_mul-15-u16-from-u128.c: New test.
    	* gcc.target/riscv/sat/sat_u_mul-15-u16-from-u32.c: New test.
    	* gcc.target/riscv/sat/sat_u_mul-15-u16-from-u64.rv32.c: New test.
    	* gcc.target/riscv/sat/sat_u_mul-15-u16-from-u64.rv64.c: New test.
    	* gcc.target/riscv/sat/sat_u_mul-15-u32-from-u128.c: New test.
    	* gcc.target/riscv/sat/sat_u_mul-15-u32-from-u64.rv32.c: New test.
    	* gcc.target/riscv/sat/sat_u_mul-15-u32-from-u64.rv64.c: New test.
    	* gcc.target/riscv/sat/sat_u_mul-15-u64-from-u128.c: New test.
    	* gcc.target/riscv/sat/sat_u_mul-15-u8-from-u128.c: New test.
    	* gcc.target/riscv/sat/sat_u_mul-15-u8-from-u16.c: New test.
    	* gcc.target/riscv/sat/sat_u_mul-15-u8-from-u32.c: New test.
    	* gcc.target/riscv/sat/sat_u_mul-15-u8-from-u64.rv32.c: New test.
    	* gcc.target/riscv/sat/sat_u_mul-15-u8-from-u64.rv64.c: New test.
    	* gcc.target/riscv/sat/sat_u_mul-run-15-u16-from-u128.c: New test.
    	* gcc.target/riscv/sat/sat_u_mul-run-15-u16-from-u32.c: New test.
    	* gcc.target/riscv/sat/sat_u_mul-run-15-u16-from-u64.c: New test.
    	* gcc.target/riscv/sat/sat_u_mul-run-15-u32-from-u128.c: New test.
    	* gcc.target/riscv/sat/sat_u_mul-run-15-u32-from-u64.c: New test.
    	* gcc.target/riscv/sat/sat_u_mul-run-15-u64-from-u128.c: New test.
    	* gcc.target/riscv/sat/sat_u_mul-run-15-u8-from-u128.c: New test.
    	* gcc.target/riscv/sat/sat_u_mul-run-15-u8-from-u16.c: New test.
    	* gcc.target/riscv/sat/sat_u_mul-run-15-u8-from-u32.c: New test.
    	* gcc.target/riscv/sat/sat_u_mul-run-15-u8-from-u64.c: New test.
    
    Signed-off-by: Pan Li <pan2.li@intel.com>

--- gcc/testsuite/gcc.target/riscv/sat/sat_arith.h
+++ gcc/testsuite/gcc.target/riscv/sat/sat_arith.h
@@ -869,4 +869,20 @@ sat_u_mul_##NT##_from_##WT##_fmt_13 (NT a, NT b) \
   sat_u_mul_##NT##_from_##WT##_fmt_13 (a, b)
 #define RUN_SAT_U_MUL_FMT_13_WRAP(NT, WT, a, b) RUN_SAT_U_MUL_FMT_13(NT, WT, a, b)
 
+#define DEF_SAT_U_MUL_FMT_14(NT, WT)             \
+NT __attribute__((noinline))                     \
+sat_u_mul_##NT##_from_##WT##_fmt_14 (NT a, NT b) \
+{                                                \
+  WT x = (WT)a * (WT)b;                          \
+  NT hi = x >> (sizeof(NT) * 8);                 \
+  NT lo = (NT)x;                                 \
+  return hi == 0 ? lo : -1;                      \
+}
+
+#define DEF_SAT_U_MUL_FMT_14_WRAP(NT, WT) DEF_SAT_U_MUL_FMT_14(NT, WT)
+#define RUN_SAT_U_MUL_FMT_14(NT, WT, a, b) \
+  sat_u_mul_##NT##_from_##WT##_fmt_14 (a, b)
+#define RUN_SAT_U_MUL_FMT_14_WRAP(NT, WT, a, b) \
+  RUN_SAT_U_MUL_FMT_14(NT, WT, a, b)
+
 #endif
