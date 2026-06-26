69fcf5817f0fbc20bbfda03e0995de6e25c1b3b6
Lucas Ly Ba <lucas.ly-ba@outlook.com>
2026-06-26 21:13:47 +0200

    gccrs: add path statement lint
    gcc/rust/ChangeLog:
    
    	* checks/lints/unused/rust-unused-checker.cc (UnusedChecker::visit):
    	New.
    	* checks/lints/unused/rust-unused-checker.h (UnusedChecker::visit):
    	New.
    	* rust-lang.cc (grs_langhook_init_options_struct): Enable warn_unused_value.
    
    gcc/testsuite/ChangeLog:
    
    	* rust/compile/path-statements_0.rs: New test.
    
    Signed-off-by: Lucas Ly Ba <lucas.ly-ba@outlook.com>

--- gcc/rust/checks/lints/unused/rust-unused-checker.cc
+++ gcc/rust/checks/lints/unused/rust-unused-checker.cc
@@ -478,5 +478,14 @@ UnusedChecker::visit (HIR::WhileLoopExpr &expr)
   walk (expr);
 }
 
+void
+UnusedChecker::visit (HIR::ExprStmt &stmt)
+{
+  if (stmt.get_expr ().get_expression_type () == HIR::Expr::ExprType::Path)
+    rust_warning_at (stmt.get_locus (), OPT_Wunused_value,
+		     "path statement with no effect");
+  walk (stmt);
+}
+
 } // namespace Analysis
 } // namespace Rust
