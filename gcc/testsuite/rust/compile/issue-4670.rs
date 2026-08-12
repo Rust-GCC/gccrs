bd2bcc47efa45f028919b61ed81798fb808a8037
w3lld1 <42353747+w3lld1@users.noreply.github.com>
2026-08-12 18:09:43 +0200

    gccrs: avoid MarkLive ICE on unresolved paths
    Name resolution reports an error for unresolved paths, but MarkLive
    still visits their lowered HIR nodes. Treat a missing definition as an
    already-diagnosed path and stop processing it instead of asserting.
    
    Fixes Rust-GCC/gccrs#4670
    
    gcc/rust/ChangeLog:
    
    	* checks/lints/rust-lint-marklive.cc (MarkLive::visit): Return on an
    	unresolved path.
    	(MarkLive::find_value_definition): Return whether lookup succeeded.
    	* checks/lints/rust-lint-marklive.h
    	(MarkLive::find_value_definition): Change return type.
    
    gcc/testsuite/ChangeLog:
    
    	* rust/compile/issue-4670.rs: New test.
    
    Signed-off-by: w3lld1 <faststepbyme@gmail.com>

---
+++ gcc/testsuite/rust/compile/issue-4670.rs
@@ -0,0 +1,16 @@
+// { dg-options "-frust-crate-attr=no_core" }
+
+pub enum Something {
+    Cat(u64),
+    Dog(u64),
+}
+
+fn main() {
+    let my_int: i32 = 1;
+
+    match my_int {
+        x if x == Something::Foo as i32 => {},
+        // { dg-error "Cannot find path .Something::Foo. in this scope" "" { target *-*-* } .-1 }
+        _ => {},
+    }
+}
