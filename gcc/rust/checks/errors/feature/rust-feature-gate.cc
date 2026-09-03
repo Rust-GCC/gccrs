5af7bb92345fe37e5e1b1b73ff76a1a40b5380e8
Arthur Cohen <arthur.cohen@opensrcsec.com>
2026-09-03 17:36:20 +0200

    feature-gate: Default-visit extern types for completeness
    gcc/rust/ChangeLog:
    
    	* checks/errors/feature/rust-feature-gate.cc (FeatureGate::visit): Add missing call to
    	default visitor for ExternType items.

--- gcc/rust/checks/errors/feature/rust-feature-gate.cc
+++ gcc/rust/checks/errors/feature/rust-feature-gate.cc
@@ -262,6 +262,8 @@ FeatureGate::visit (AST::ExternalTypeItem &item)
   // `AST::ExternalTypeItem`.
   gate (Feature::Name::EXTERN_TYPES, item.get_locus (),
 	"extern types are experimental");
+
+  AST::DefaultASTVisitor::visit (item);
 }
 
 void
